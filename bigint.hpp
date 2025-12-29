987036be478fb89080db560d3595d95c3a910593
ming_dude <mingde@gmail.com>
2025-12-29 22:33:31 +0800

    Update bigint.hpp

--- bigint.hpp
+++ bigint.hpp
@@ -38,3 +38,59 @@ public:
 
 std::ostream &operator<<(std::ostream &out, const bigint &b);
 #endif // BIGINT_HPP
+
+
+// #ifndef BIGINT_HPP
+// #define BIGINT_HPP
+
+// #include <string>
+// #include <iostream>
+
+// class bigint
+// {
+//     std::string digit;
+
+//     void removeZero();
+// public:
+//     explicit bigint(unsigned long int n = 0);
+//     bigint (const bigint & other);
+
+//     ~bigint() {};
+
+//     bigint operator=(const bigint & rhs);
+
+//     bigint & operator+=(const bigint & rhs); // return & 
+//     bigint & operator+=(unsigned long int n);
+
+//     bigint operator+(const bigint & rhs) const;
+//     bigint operator+(unsigned long int n) const;
+
+//     bool operator==(const bigint & rhs) const;
+//     bool operator==(unsigned long int n) const;
+
+//     bool operator!=(const bigint & rhs) const;
+//     bool operator!=(unsigned long int n) const;
+
+//     bool operator>(const bigint & rhs) const;
+//     bool operator>(unsigned long int n) const;
+
+//     bool operator<(const bigint & rhs) const;
+//     bool operator<(unsigned long int n) const;
+
+//     bool operator>=(const bigint & rhs) const;
+//     bool operator>=(unsigned long int n) const;
+
+//     bool operator<=(const bigint & rhs) const;
+//     bool operator<=(unsigned long int n) const;
+
+//     bigint & operator++();
+//     const bigint operator++(int);
+
+//     bigint operator<<(unsigned long int n);
+//     bigint operator>>(unsigned long int n);
+
+//     friend std::ostream & operator<<(std::ostream out, const bigint & v);
+
+// };
+
+// #endif
